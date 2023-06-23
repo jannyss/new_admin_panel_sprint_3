c17f087addae0f969695e8e3095cc88c39367913
Evgeniya Shchepina <e.shchepina@corp.vk.com>
2023-06-23 09:28:59 +0300

    Update init_source_db.sql

--- init_scripts/init_source_db.sql
+++ init_scripts/init_source_db.sql
@@ -1,15 +1,74 @@
-CREATE TABLE IF NOT EXISTS "movies" (
-  "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-  "title" VARCHAR(255) NOT NULL,
-  "description" TEXT,
-  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE SCHEMA IF NOT EXISTS "content";
+CREATE TABLE IF NOT EXISTS "content"."film_work" (
+    "id" uuid PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "creation_date" DATE,
+    "rating" FLOAT,
+    "type" TEXT NOT NULL,
+    "file_path" TEXT,
+    "created_at" timestamp with time zone NOT NULL,
+    "updated_at" timestamp with time zone NOT NULL
 );
-CREATE INDEX "movies_id-idx" ON "movies"("id");
-CREATE INDEX "movies_updated_at-idx" ON "movies"("updated_at");
 
+CREATE TABLE IF NOT EXISTS "content"."genre" (
+    "id" uuid PRIMARY KEY,
+    "name" TEXT NOT NULL UNIQUE,
+    "description" TEXT,
+    "created_at" timestamp with time zone NOT NULL,
+    "updated_at" timestamp with time zone NOT NULL
+);
 
-CREATE OR REPLACE  FUNCTION update_updated_at_movie() RETURNS TRIGGER
+CREATE TABLE IF NOT EXISTS "content"."person" (
+    "id" uuid PRIMARY KEY,
+    "full_name" TEXT NOT NULL UNIQUE,
+    "created_at" timestamp with time zone NOT NULL,
+    "updated_at" timestamp with time zone NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "content"."genre_film_work" (
+    "id" uuid PRIMARY KEY,
+    "genre_id" uuid NOT NULL,
+    "film_work_id" uuid NOT NULL,
+    "created_at" timestamp with time zone NOT NULL,
+    CONSTRAINT fk_genre_id
+    FOREIGN KEY (genre_id)
+    REFERENCES content.genre (id)
+    ON DELETE CASCADE,
+    CONSTRAINT fk_film_work_id_genre
+    FOREIGN KEY (film_work_id)
+    REFERENCES content.film_work (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "content"."person_film_work" (
+    "id" uuid PRIMARY KEY,
+    "person_id" uuid NOT NULL,
+    "film_work_id" uuid NOT NULL,
+	"role" TEXT NOT NULL,
+    "created_at" timestamp with time zone NOT NULL,
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES content.person (id)
+    ON DELETE CASCADE,
+    CONSTRAINT fk_film_work_id_person
+    FOREIGN KEY (film_work_id)
+    REFERENCES content.film_work (id)
+    ON DELETE CASCADE
+);
+
+CREATE INDEX "genre_film_work_film_work_id_idx" ON "content"."genre_film_work" ("film_work_id");
+CREATE INDEX "genre_film_work_genre_id_idx" ON "content"."genre_film_work" ("genre_id");
+
+CREATE INDEX "person_film_work_film_work_id_idx" ON "content"."person_film_work" ("film_work_id");
+CREATE INDEX "person_film_work_person_id_idx" ON "content"."person_film_work" ("person_id");
+
+CREATE UNIQUE INDEX "film_work_genre_idx" ON "content"."genre_film_work" ("film_work_id", "genre_id");
+
+CREATE UNIQUE INDEX "film_work_title_creation_date_idx" ON "content"."film_work" ("title", "creation_date");
+CREATE INDEX "film_work_updated_at-idx" ON "content"."film_work" ("updated_at");
+
+CREATE OR REPLACE  FUNCTION update_updated_at_film_work() RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
     BEGIN
@@ -22,6 +81,6 @@ $$;
 CREATE TRIGGER update_user_task_updated_on
     BEFORE UPDATE
     ON
-        public.movies
+        content.film_work
     FOR EACH ROW
-EXECUTE PROCEDURE update_updated_at_movie();
+EXECUTE PROCEDURE update_updated_at_film_work();
